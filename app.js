53abb9c04e5882d872d6d51d2db1a582b1ce991e
abuameen4g <shahedkhan1970@yahoo.com>
2023-11-26 08:00:30 +0000

    Code Update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let array_who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  let array_action = ['ate', 'peed', 'crushed', 'broke'];
+  let array_what = ['my homework', 'the keys', 'the car'];
+  let array_when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+  var randomword_who = array_who[Math.floor(Math.random() * array_who.length)];
+  var randomword_action = array_action[Math.floor(Math.random() * array_action.length)];
+  var randomword_what = array_what[Math.floor(Math.random() * array_what.length)];
+  var randomword_when = array_when[Math.floor(Math.random() * array_when.length)];
+
+  var random_sentence = randomword_who + " " + randomword_action + " " + randomword_what + " " + randomword_when;
+  console.log(random_sentence);
+
+  document.getElementById("excuse").innerHTML = random_sentence;
+
 };
